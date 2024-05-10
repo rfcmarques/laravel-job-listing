5c0e47b57ecf74e4ef38c30b0cd3ef129a7ea82d
Rui Marques <a.marques1967@gmail.com>
2024-05-10 17:03:56 +0100

    Added the fillable fields

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,6 +11,8 @@ class Job extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title', 'salary', 'location', 'schedule', 'url', 'tags', 'featured'];
+
     public function tag(string $name): void
     {
         $tag = Tag::firstOrCreate(['name' => $name]);
