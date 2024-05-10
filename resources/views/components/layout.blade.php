2b0085589f002b1955f325d69b002cc4a203fa16
Rui Marques <a.marques1967@gmail.com>
2024-05-10 14:39:12 +0100

    Changes regarding authentication

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,9 +27,19 @@
                 <a href="#">Salaries</a>
                 <a href="#">Companies</a>
             </div>
-            <div>
-                <a href="#">Post a job</a>
-            </div>
+
+            @auth
+                <div>
+                    <a href="/jobs/create">Post a job</a>
+                </div>
+            @endauth
+
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Log In</a>
+                </div>
+            @endguest
         </nav>
 
         <main class="mt-10 max-w-[986px] mx-auto">
